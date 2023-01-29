0a3cea75dabe5241858ff694da980c9a54c85661
Colin Roberts <colin.roberts@veelogic.co.uk>
2023-01-29 11:28:13 +0000

    3 more tests added to check the other items Value when going into the basket

--- CheckoutKata.Tests/Checkout/CheckoutTests.cs
+++ CheckoutKata.Tests/Checkout/CheckoutTests.cs
@@ -36,6 +36,74 @@ namespace CheckoutKata.Tests.Checkout
             const int expectedBasketItemValue = 10;
             result.Should().Be(expectedBasketItemValue);
         }
-    }
 
+
+        [Fact]
+        public void Given_CheckService_AddItemBToBasket_When_Invoked_ExpectedItemBValuesCorrect()
+        {
+            //Arrange
+            var itemB = new Item
+            {
+                SKU = ItemSkus.ItemBSku,
+                Price = ItemPrices.ItemBPrice,
+            };
+
+            _sut.AddItemToBasket(itemB);
+
+            //Act
+
+            var result = _sut.CalculateBasketTotal();
+
+
+            //Assert
+            const int expectedBasketItemValue = 15;
+            result.Should().Be(expectedBasketItemValue);
+        }
+
+        [Fact]
+        public void Given_CheckService_AddItemCToBasket_When_Invoked_ExpectedItemCValuesCorrect()
+        {
+            //Arrange
+            var itemA = new Item
+            {
+                SKU = ItemSkus.ItemCSku,
+                Price = ItemPrices.ItemCPrice,
+            };
+
+            _sut.AddItemToBasket(itemA);
+
+            //Act
+
+            var result = _sut.CalculateBasketTotal();
+
+
+            //Assert
+            const int expectedBasketItemValue = 40;
+            result.Should().Be(expectedBasketItemValue);
+        }
+
+        [Fact]
+        public void Given_CheckService_AddItemDToBasket_When_Invoked_ExpectedItemDValuesCorrect()
+        {
+            //Arrange
+            var itemA = new Item
+            {
+                SKU = ItemSkus.ItemDSku,
+                Price = ItemPrices.ItemDPrice,
+            };
+
+            _sut.AddItemToBasket(itemA);
+
+            //Act
+
+            var result = _sut.CalculateBasketTotal();
+
+
+            //Assert
+            const int expectedBasketItemValue = 55;
+            result.Should().Be(expectedBasketItemValue);
+        }
+
+
+    }
 }
