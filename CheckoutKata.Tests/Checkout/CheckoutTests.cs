62a5ad9999050061cd34095e46556f024293fbfc
Colin Roberts <colin.roberts@veelogic.co.uk>
2023-01-29 11:23:48 +0000

    Add first test to make sure Item A adds to basket and price is correct when doing so

--- CheckoutKata.Tests/Checkout/CheckoutTests.cs
+++ CheckoutKata.Tests/Checkout/CheckoutTests.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CheckoutKata.Checkout;
+using CheckoutKata.ShoppingItem;
+using CheckoutKata.ShoppingItem.Item;
+using FluentAssertions;
+using Xunit;
 
 namespace CheckoutKata.Tests.Checkout
 {
-    class CheckoutTests
+    public class CheckoutTests
     {
+        private readonly CheckoutService _sut;
+
+        public CheckoutTests()
+        {
+            _sut = new CheckoutService();
+        }
+
+        [Fact]
+        public void Given_CheckService_AddItemAToBasket_When_Invoked_ExpectedItemAValuesCorrect()
+        {
+            //Arrange
+            var itemA = new Item
+            {
+                SKU = ItemSkus.ItemASku,
+                Price = ItemPrices.ItemAPrice,
+            };
+
+            _sut.AddItemToBasket(itemA);
+
+            //Act
+
+            var result = _sut.CalculateBasketTotal();
+
+
+            //Assert
+            const int expectedBasketItemValue = 10;
+            result.Should().Be(expectedBasketItemValue);
+        }
     }
+
 }
