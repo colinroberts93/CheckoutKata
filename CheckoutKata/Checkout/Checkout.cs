2acfc1bc617df0365073bfb409c23d011ec4bffb
Colin Roberts <colin.roberts@veelogic.co.uk>
2023-01-29 10:51:55 +0000

    Implement add to basket

---
+++ CheckoutKata/Checkout/Checkout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CheckoutKata.ShoppingItem.Item;
+
+namespace CheckoutKata.Checkout
+{
+    public class CheckoutService : ICheckout
+    {
+        public List<Item> BasketItems { get; } = new List<Item>();
+
+        public void AddItemToBasket(Item item)
+        {
+            BasketItems.Add(item);
+        }
+
+
+    }
+}
---
+++ CheckoutKata/Checkout/ICheckout.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CheckoutKata.ShoppingItem.Item;
+
+namespace CheckoutKata.Checkout
+{
+    public interface ICheckout
+    {
+        public List<Item> BasketItems { get; }
+        public void AddItemToBasket(Item item);
+    }
+}
